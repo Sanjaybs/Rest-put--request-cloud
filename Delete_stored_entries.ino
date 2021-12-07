d6e1870551b09172b5fe660a3e37c873bf4e87ed
Sanjaybs <sanjaybs.134@gmail.com>
2021-12-07 15:11:40 +0100

    Add files via upload

---
+++ Delete_stored_entries.ino
@@ -0,0 +1,29 @@
+#include <Process.h>
+void setup() {
+  // put your setup code here, to run once:
+Bridge.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int value = analogRead(A0);
+
+String deviceId = "<device ID>"; // Change this to the name of your arduino
+String url = "<insert your url here>";
+
+Process p;
+
+p.begin("curl");
+p.addParameter("-k");  // allow https without certificate checking
+p.addParameter("-XPUT");  // initiate a PUT request
+p.addParameter("--data");
+p.addParameter((String) value);
+p.addParameter("--url");
+p.addParameter(url);
+p.addParameter("-X");
+p.addParameter("DELETE");
+p.run();
+
+Serial.print("Exit: ");
+Serial.println(p.exitValue());
+}
